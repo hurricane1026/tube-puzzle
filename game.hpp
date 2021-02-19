0329425e8cccba1db8b1d467d706170db65a0b92
hurricane1026 <hurricane1026@gmail.com>
2021-02-19 11:35:00 +0800

    make game.hpp and its tests works

--- game.hpp
+++ game.hpp
@@ -6,33 +6,70 @@
 
 #include "tube.hpp"
 #include "fmt/core.h"
+#include "yaml-cpp/yaml.h"
+#include <boost/lexical_cast.hpp>
+#include <vector>
 
-template<std::uint16_t N, std::uint8_t C>
+using boost::lexical_cast;
+using boost::bad_lexical_cast;
+
+template<std::uint8_t C>
 class game {
  public:
-  inline game(const std::string& path) {
+  inline game():
+    tubes_(),
+    hashcodes_()
+  {
+    YAML::Node game_node = YAML::LoadFile("game.yaml");
+    auto node = game_node["empty-tube"];
+    std::string empty_tube = node.as<std::string>();
 
+    auto tubes = game_node["tubes"];
+    try {
+      empty_tube_num_ = boost::lexical_cast<unsigned int>(empty_tube);
+      fmt::print("free_empty_tube_num is {}", empty_tube_num_);
+
+
+      uint16_t no = 0;
+      for (auto iter = tubes.begin(); iter != tubes.end(); ++iter) {
+        std::string content = iter->as<std::string>();
+        tube<C>* temp_tube_ptr = new tube<C>(no++, content);
+        tubes_.push_back(temp_tube_ptr);
+        fmt::print(content);
+      }
+
+      for (unsigned int empty_no = 0; empty_no < empty_tube_num_; ++empty_no)
+      {
+        tubes_.push_back(new tube<C>(no++));
+      }
+
+      hash_record();
+
+    }
+    catch(bad_lexical_cast& e){
+      fmt::print(e.what());
+    }
   }
 
   inline game(const game& g):
-    tubes(),
-    hashcodes(g.hashcodes)
+    tubes_(),
+    hashcodes_(g.hashcodes)
   {
     for (auto t : g.tubes) {
       tube<C>* new_t = new tube<C>(*t);
-      tubes.push_back(new_t);
+      tubes_.push_back(new_t);
     }
   }
 
   inline ~game() {
-    for (auto t : tubes) {
+    for (auto t : tubes_) {
       delete t;
     }
   }
 
   inline std::vector<tube<C>> availables() const {
     std::vector<tube<C>*> avails;
-    for (auto t : tubes) {
+    for (auto t : tubes_) {
       if (!t->is_pure()) {
         avails.push_back(t);
       }
@@ -42,7 +79,7 @@ class game {
 
   inline std::string genhash() const {
      std::string code;
-    for (auto & c : hashcodes) {
+    for (auto & c : hashcodes_) {
       code += "|";
       code += c;
     }
@@ -51,23 +88,23 @@ class game {
 
   inline bool hash_record() {
     std::string code = genhash();
-    if (hashcodes.contains(code)) {
+    if (hashcodes_.contains(code)) {
       return false;
     } else {
-      hashcodes.insert(code);
+      hashcodes_.insert(code);
       return true;
     }
   }
 
   inline void show_codes() const {
-    for (auto & code : hashcodes) {
+    for (auto & code : hashcodes_) {
       fmt::print("{}\n", code);
     }
   }
 
   inline decltype(auto) future_actions() {
     std::vector<std::pair<std::uint8_t, std::uint8_t>> actions;
-    for (auto src : tubes) {
+    for (auto src : tubes_) {
       auto [src_slot, src_color] = src->top_slots();
       if (src_color == 0) continue;
       //std::uint16_t curr_no = src->get_no();
@@ -85,14 +122,14 @@ class game {
 
   inline bool do_fill(std::pair<std::uint8_t, std::uint8_t>& action) {
     auto [src, dst] = action;
-    assert(tubes[src]->get_no() == src);
-    assert(tubes[dst]->get_no() == dst);
-    tubes[src]->fill(*tubes[dst]);
+    assert(tubes_[src]->get_no() == src);
+    assert(tubes_[dst]->get_no() == dst);
+    tubes_[src]->fill(*tubes_[dst]);
     return hash_record();
   }
 
   inline bool success() const {
-    for (auto t : tubes) {
+    for (auto t : tubes_) {
       if (!t->pure() && !t->empty())
         return false;
     }
@@ -100,8 +137,9 @@ class game {
   }
 
  private:
-  std::vector<tube<C>*> tubes;
-  std::set<std::string> hashcodes;
+  unsigned int empty_tube_num_;
+  std::vector<tube<C>*> tubes_;
+  std::set<std::string> hashcodes_;
 };
 
 
--- tests/game_test.cpp
+++ tests/game_test.cpp
@@ -2,3 +2,15 @@
  * Copyright (C) 2021 hurricane <l@stdb.io>. All rights reserved.
  */
 #include "game.hpp"
+#include <gtest/gtest.h>
+
+
+
+class game_test : public ::testing::Test {
+ protected:
+   game<4> g;
+};
+
+TEST_F(game_test, smoke_Test) {
+  g.show_codes();
+};
